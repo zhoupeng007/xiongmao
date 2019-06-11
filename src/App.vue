54a98fae6c3c26d2842ab7391dfa2a23aba4c9ca
Your Name <you@example.com>
2019-06-11 20:45:12 +0800

    第一次实验

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <ul>
+      <li v-for="data in datalist" :key="data.commodityId">
+        <img :src="data.image" alt="">
+        <h3>{{data.title}}</h3>
+      </li>
+    </ul>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      datalist: []
+    }
+  },
+  mounted () {
+    axios.get('/api/tab/1?start=0').then(res => {
+      console.log(res.data.data.items.list)
+      this.datalist = res.data.data.items.list
+    })
+  }
+  // https://img.alicdn.com/bao/uploaded/i3/3878483432/TB1dYSVmAKWBuNjy1zjXXcOypXa_!!0-item_pic.jpg
 
+}
+</script>
+
+<style lang="scss">
+img{
+  width: 100%;
+  height: 100%;
+}
 </style>
