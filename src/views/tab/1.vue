151151f61de4f661733e96fa96b7b53a9cfb3e2e
Your Name <you@example.com>
2019-06-13 16:27:37 +0800

    bug已修复首页hostlist组件待更新

--- src/views/tab/1.vue
+++ src/views/tab/1.vue
@@ -2,18 +2,15 @@
   <div class="film">
     <banner></banner>
     <grids></grids>
-    <lun></lun>
   </div>
 </template>
 <script>
 import banner from '@/views/tab/banner'
 import grids from '@/views/tab/film/grids'
-import lun from '@/views/tab/film/lun'
 export default {
   components: {
     banner,
-    grids,
-    lun
+    grids
   }
 }
 </script>
